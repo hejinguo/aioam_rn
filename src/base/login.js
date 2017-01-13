f0ca0b68099a04198234431b88a6e9264b53b561
hejg <hejinguo727@163.com>
2017-01-13 12:02:36 +0800

    react-native-elements

--- src/base/login.js
+++ src/base/login.js
@@ -5,16 +5,8 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Image, View, TextInput, Alert,TouchableOpacity} from 'react-native';
-import {
-    Button,
-    Icon,
-    Text,
-    InputGroup,
-    Input,
-    List,
-    ListItem
-} from 'native-base';
+import {StyleSheet, Image, View, TextInput, Alert, TouchableOpacity} from 'react-native';
+import {Button, FormLabel, FormInput, Text, Icon} from 'react-native-elements';
 
 import main from './main';
 
@@ -27,43 +19,38 @@ export default class login extends Component {
             loginMark: ''
         };
     }
-    _handleLogin(){
+
+    _handleLogin() {
         this.props.navigator.push({
-            component:main
+            component: main
         })
     }
+
     render() {
         return (
             <View style={styles.container}>
                 {/*<Image source={require('./img/app-login-logo.png')} style={styles.logo}/>*/}
                 <View style={styles.aheader}>
-                    <Text style={{color: '#000000',textAlign:'center'}}>
-                        <Icon name="ios-unlock" style={{ color: '#5CB85C'}} />
+                    <Text style={{color: '#5CB85C',textAlign:'center'}}>
+                        ICON
                     </Text>
                 </View>
                 <View style={styles.abody}>
-                    <List>
-                        <ListItem>
-                            <InputGroup>
-                                <Icon name="ios-person" style={{ color: '#5CB85C' }} />
-                                <Input placeholder="请输入工号" />
-                            </InputGroup>
-                        </ListItem>
-                        <ListItem>
-                            <InputGroup>
-                                <Icon name="ios-unlock" style={{ color: '#5CB85C' }} />
-                                <Input placeholder="请输入密码" secureTextEntry />
-                            </InputGroup>
-                        </ListItem>
-                        <ListItem>
-                            <InputGroup>
-                                <Icon name="ios-call" style={{ color: '#5CB85C' }} />
-                                <Input placeholder="请输入验证码" keyboardType="numeric" />
-                            </InputGroup>
-                        </ListItem>
-                    </List>
-                    <TouchableOpacity style={{marginTop: 10,alignItems:'flex-end'}}><Text>获取验证码</Text></TouchableOpacity>
-                    <Button block success style={{marginTop: 10, marginBottom: 10 }} onPress={() => this._handleLogin()}>登录</Button>
+                    <FormLabel>工号</FormLabel>
+                    <FormInput placeholder="请输入工号"/>
+                    <FormLabel>密码</FormLabel>
+                    <FormInput placeholder="请输入密码" secureTextEntry/>
+                    <FormLabel>验证码</FormLabel>
+                    <FormInput placeholder="请输入验证码"/>
+                    <TouchableOpacity style={styles.sendmark}>
+                        <Text style={{color:'#86939E'}}>获取验证码</Text>
+                    </TouchableOpacity>
+                    <Button
+                        raised
+                        icon={{name: 'person'}}
+                        title='登录'
+                        backgroundColor="#5CB85C"
+                        onPress={this._handleLogin.bind(this)}/>
                 </View>
                 <Text style={styles.afooter}>Copyright © 2010-2017 ESOP</Text>
             </View>
@@ -82,7 +69,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     abody: {
-        flex: 2,
+        flex: 3,
         alignItems: 'stretch',
         padding: 10
     },
@@ -91,5 +78,11 @@ const styles = StyleSheet.create({
         padding: 5,
         color: '#666666',
         textAlign: 'center'
+    },
+    sendmark:{
+        marginTop: 10,
+        marginBottom:10,
+        marginRight:20,
+        alignItems:'flex-end'
     }
 });
