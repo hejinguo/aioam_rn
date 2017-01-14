4834cd87aeb1f327b95ece18a3becae807d35480
hejg <hejinguo727@163.com>
2017-01-14 12:38:33 +0800

    login page style

--- src/base/login.js
+++ src/base/login.js
@@ -29,29 +29,28 @@ export default class login extends Component {
     render() {
         return (
             <View style={styles.acontainer}>
-                {/*<Image source={require('./img/app-login-logo.png')} style={styles.logo}/>*/}
                 <View style={styles.aheader}>
-                    <Text style={{color: '#5CB85C',textAlign:'center'}}>
-                        ICON
+                    <Text style={{color:'#00BFBE',fontSize:16}}>
+                        ESOP+运维宝
                     </Text>
                 </View>
                 <View style={styles.abody}>
-                    <FormLabel>工号</FormLabel>
-                    <FormInput placeholder="请输入工号"/>
-                    <FormLabel>密码</FormLabel>
-                    <FormInput placeholder="请输入密码" secureTextEntry/>
-                    <FormLabel>验证码</FormLabel>
-                    <FormInput placeholder="请输入验证码"/>
-                    <TouchableOpacity style={styles.sendmark}>
-                        <Text style={{color:'#86939E'}}>获取验证码</Text>
+                    <FormLabel labelStyle={styles.formlabel}>工号</FormLabel>
+                    <FormInput placeholder="请输入工号" value={this.state.loginCode} placeholderTextColor="#444354"/>
+                    <FormLabel labelStyle={styles.formlabel}>密码</FormLabel>
+                    <FormInput placeholder="请输入密码" secureTextEntry placeholderTextColor="#444354"/>
+                    <FormLabel labelStyle={styles.formlabel}>验证码</FormLabel>
+                    <FormInput placeholder="请输入验证码" placeholderTextColor="#444354"/>
+                    <TouchableOpacity style={styles.sendmark} onPress={this._handleLogin.bind(this)}>
+                        <Text style={{color:'#FFFFFF'}}>获取验证码</Text>
                     </TouchableOpacity>
                     <Button
                         raised
                         title='登录'
-                        backgroundColor="#5CB85C"
+                        backgroundColor="#00BFBE"
                         onPress={this._handleLogin.bind(this)}/>
                 </View>
-                <Text style={styles.afooter}>Copyright © 2010-2017 ESOP</Text>
+                <Text style={styles.afooter}>Copyright © 2010-2017 ESOP TEAM</Text>
             </View>
         );
     }
@@ -61,11 +60,12 @@ const styles = StyleSheet.create({
     acontainer: {
         flex: 1,
         alignItems: 'stretch',
-        /*backgroundColor: '#F5F5F5'*/
+        backgroundColor: '#252932'
     },
     aheader: {
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     abody: {
         flex: 3,
@@ -75,7 +75,7 @@ const styles = StyleSheet.create({
     afooter: {
         fontSize: 10,
         padding: 5,
-        color: '#666666',
+        color: '#86939E',
         textAlign: 'center'
     },
     sendmark: {
@@ -83,5 +83,12 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         marginRight: 20,
         alignItems: 'flex-end'
+    },
+    formlabel: {
+        color:'#86939E'
+    },
+    forminput: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#86939E'
     }
 });
--- src/base/main.js
+++ src/base/main.js
@@ -4,19 +4,29 @@
 
 import React, {Component} from 'react';
 import {Alert, Text, View} from 'react-native';
-import {SearchBar} from 'react-native-elements';
+import {SearchBar, ButtonGroup} from 'react-native-elements';
 
 export default class main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            searchContent: ''
+            selectedIndex: 2
         };
     }
 
     render() {
+        const buttons = ['Hello', 'World', 'Buttons'];
+        const selectedIndex = this.state.selectedIndex;
         return (
-        <SearchBar lightTheme placeholder='请输入工号或集团的关键字检索...' />
-        )
+            <View>
+        <SearchBar lightTheme placeholder='请输入工号或集团的关键字检索...'/>
+
+        <ButtonGroup
+            onPress={(index) => {this.setState({selectedIndex:index})}}
+            containerStyle={{height:30}}
+            selectedIndex={selectedIndex}
+            buttons={buttons} />
+            </View>
+    )
     }
 }
