7418281bb0e660f15cdbf42ab3ab5efe2bef1972
hejg <hejinguo727@163.com>
2017-01-15 19:45:21 +0800

    AIListView refresh loadmore

--- src/utils/components/AIListView.js
+++ src/utils/components/AIListView.js
@@ -7,7 +7,9 @@ import {Alert, Text, View, ListView,RefreshControl, StyleSheet} from 'react-nati
 import {Icon, List} from 'react-native-elements';
 import util from '../util';
 
-export default class InterLoaded extends Component {
+var paramData = { opTime: '20170114', pageNo: 1, pageSize: 10, total: 10 };
+
+export default class AIListView extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -16,7 +18,9 @@ export default class InterLoaded extends Component {
             }),
             loaded: false
         };
-        this._fetchData = this._fetchData.bind(this);
+        this._refreshData = this._refreshData.bind(this);
+        this._endLoadMore = this._endLoadMore.bind(this);
+        this._data = [];//缓存页面数据
     }
 
     componentDidMount() {
@@ -24,43 +28,58 @@ export default class InterLoaded extends Component {
     }
 
     _fetchData() {
-        let paramData = { opTime: '20170114', pageNo: 1, pageSize: 10, total: 10 };
         var that = this;
         util.ajax(this.props.remoteAddr, paramData, function (data) {
             if (data.state) {
+                that._data = that._data.concat(data.info.rows || []);
                 that.setState({
-                    dataSource: that.state.dataSource.cloneWithRows(data.info.rows || []),
+                    dataSource: that.state.dataSource.cloneWithRows(that._data),
                     loaded: true
                 });
             }
         });
     }
+    _refreshData() {
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows([]),
+            loaded: false
+        });
+        this._fetchData();
+    }
+    _endLoadMore() {
+        if(this.state.loaded){
+            paramData.pageNo++;
+            this._fetchData();
+        }
+    }
 
     render() {
         if (!this.state.loaded) {
             return (
-                <Text>Loading...</Text>
+                <View>
+                    <Text style={{textAlign:'center'}}>Loading...</Text>
+                </View>
             );
         } else {
             return (
-                <List containerStyle={{marginTop:1}}>
-                    <ListView
-                        dataSource={this.state.dataSource}
-                        renderRow={this.props.renderRow}
-                        onEndReached={console.log("11111")}
-                        refreshControl={
-                            <RefreshControl
-                                refreshing={false}
-                                onRefresh={() => {}}
-                                tintColor="#ff0000"
-                                title="Loading..."
-                                titleColor="#00ff00"
-                                colors={['#ff0000', '#00ff00', '#0000ff']}
-                                progressBackgroundColor="#ffff00"
-                            />
-                        }
-                    />
-                </List>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this.props.renderRow}
+                    onEndReached={this._endLoadMore}
+                    onEndReachedThreshold={5}
+                    enableEmptySections
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={false}
+                            onRefresh={this._refreshData}
+                            tintColor="#ff0000"
+                            title="Loading..."
+                            titleColor="#00ff00"
+                            colors={['#ff0000', '#00ff00', '#0000ff']}
+                            progressBackgroundColor="#ffff00"
+                        />
+                    }
+                />
             )
         }
     }
